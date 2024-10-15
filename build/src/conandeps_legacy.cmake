be718803f59e861ccebb8e634f38dcd298787c28
Blud-X <arimahardika7789@gmail.com>
2024-10-15 18:57:28 +0800

    first commit

--- build/src/conandeps_legacy.cmake
+++
@@ -1,7 +0,0 @@
-message(STATUS "Conan: Using CMakeDeps conandeps_legacy.cmake aggregator via include()")
-message(STATUS "Conan: It is recommended to use explicit find_package() per dependency instead")
-
-find_package(fmt)
-find_package(magic_enum)
-
-set(CONANDEPS_LEGACY  fmt::fmt  magic_enum::magic_enum )
